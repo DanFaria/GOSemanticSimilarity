df995c9ae1c4b36fc561cfe327ab716b5aa188af
blima <blima@vienna>
2019-04-12 15:35:12 +0100

    Added GOType.java

---
+++ src/ontology/GOType.java
@@ -0,0 +1,47 @@
+package ontology;
+
+public enum GOType
+{
+	MF ("molecular_function", "MF", "F"),
+	BP ("biological_process", "BP", "P"),
+	CC ("cellular_component", "CC", "C");
+
+	private String label;
+	private String accronym;
+	private String code;
+	
+	private GOType(String l, String a, String c)
+	{
+		label = l;
+		accronym = a;
+		code = c;
+	}
+	
+	public static GOType parse(String s)
+	{
+		if(s.length() == 1)
+		{
+			for(GOType t : GOType.values())
+				if(s.equals(t.code))
+					return t;
+		}
+		else if(s.length() == 2)
+		{
+			for(GOType t : GOType.values())
+				if(s.equals(t.accronym))
+					return t;			
+		}
+		else if(s.length() == 18)
+		{
+			for(GOType t : GOType.values())
+				if(s.equals(t.code))
+					return t;			
+		}
+		return null;
+	}	
+
+	public String toString()
+	{
+		return label;
+	}
+}
