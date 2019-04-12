e9acc6928a10595dde13adcec47a0497b8838cc7
blima <blima@vienna>
2019-04-12 15:31:35 +0100

    Added Maximum.java

---
+++ src/metrics/Maximum.java
@@ -0,0 +1,38 @@
+package metrics;
+
+import main.Main;
+
+public class Maximum implements GeneSimilarityMetric 
+{
+	private TermSimilarityMetric measure;
+	
+	public Maximum(boolean structural, TermSimilarityMetric measure)
+	{
+		this.measure = measure;
+	}
+	
+	public double getGeneSimilarity(String gene1, String gene2) 
+	{
+		if(gene1.equals(gene2))
+			return 1;
+		else
+		{
+			double max= 0;
+
+			for (String t1: Main.getGO().getNonRedundantTerms(gene1))
+			{				
+				for (String t2:  Main.getGO().getNonRedundantTerms(gene2))
+				{
+					double score = measure.getTermSimilarity(t1, t2);
+					if (max > score)
+					{
+						max = score;
+					}
+				}
+
+			}
+			return max;
+		}
+	}
+
+}
