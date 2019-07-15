52d5cf77deeb8f39f2cf4bd569a6068da4196f05
blima <blima@vienna>
2019-07-15 14:35:22 +0100

    Added shortcut for similarity score

--- src/metrics/JiangConrath.java
+++ src/metrics/JiangConrath.java
@@ -9,7 +9,18 @@ public class JiangConrath implements TermSimilarityMetric
 		if(term1.equals(term2))
 			return 1.0;
 		else
-			return 1-Main.getGO().getInfoContent(term1) + Main.getGO().getInfoContent(term2) -
-					2*Main.getGO().getInfoContent(Main.getGO().getMICA(term1,term2));
+		{
+			double ic1 = Main.getGO().getInfoContent(term1);
+			double ic2 = Main.getGO().getInfoContent(term2);
+			double mica = Main.getGO().getInfoContent(Main.getGO().getMICA(term1,term2));
+
+			if (ic1 == 0 && ic2 == 0 && mica == 0)
+			{
+				return 0.0;
+			}
+			
+			return 1-(ic1 + ic2-2*mica);
+		}
 	}
 }
+
