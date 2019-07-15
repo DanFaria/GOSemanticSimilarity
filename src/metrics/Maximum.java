bc25e98b8ca5e2ca3814fdc5327f268cd3ed1967
blima <blima@vienna>
2019-07-15 14:49:55 +0100

    Added GO type specification functionality

--- src/metrics/Maximum.java
+++ src/metrics/Maximum.java
@@ -1,35 +1,49 @@
 package metrics;
 
-import main.Main;
+import java.util.Set;
 
-public class Maximum implements GeneSimilarityMetric 
+import main.Main;
+import ontology.GOType;
+
+public class Maximum implements GeneSimilarityMetric
 {
 	private TermSimilarityMetric measure;
-	
+
 	public Maximum(TermSimilarityMetric measure)
 	{
 		this.measure = measure;
 	}
-	
+
 	public double getGeneSimilarity(String gene1, String gene2) 
 	{
 		if(gene1.equals(gene2))
 			return 1;
 		else
 		{
-			double max= 0;
+			double max= 0.0;
+			Set<String> set1;
+			Set<String> set2;
+			GOType t = Main.getType();
+			if(t == null)
+			{
+				set1 = Main.getGO().getAnnotationsGene(gene1);
+				set2 = Main.getGO().getAnnotationsGene(gene2);
+			}
+			else
+			{
+				set1 = Main.getGO().getAnnotationsGene(gene1,t);
+				set2 = Main.getGO().getAnnotationsGene(gene2,t);
+			}
 
-			for (String t1: Main.getGO().getNonRedundantTerms(gene1))
+			for (String t1: set1)
 			{				
-				for (String t2:  Main.getGO().getNonRedundantTerms(gene2))
+				for (String t2:  set2)
 				{
 					double score = measure.getTermSimilarity(t1, t2);
-					if (max > score)
-					{
-						max = score;
-					}
-				}
 
+					if (max < score)
+						max = score;
+				}
 			}
 			return max;
 		}
